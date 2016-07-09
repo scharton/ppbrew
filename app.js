5e08004080331b56471f5e0c8652324669c4591e
Derek Scharton <scharton@gmail.com>
2016-07-09 01:07:30 +0000

    add Instagram and Facebook logos

--- app.js
+++ app.js
@@ -1,8 +1,19 @@
 (function() {
   'use strict';
  var app = angular.module('app', []);
-  app.controller('ctrl', function($scope) {
-    $scope.name = "Pine and Palm";
+  app.controller('ctrl', function() {
+    var vm = this;
+    vm.name = "Pine and Palm Brewing";
+    vm.addr = {
+      street: "352 W. Bedford Ave., #111",
+      city: "Fresno",
+      state: "CA",
+      zip: "93711"
+    };
+    vm.phone = "(559) 970-7580";
+    vm.email = "pineandpalmbrewing@gmail.com";
+    vm.facebook = "https://www.facebook.com/PineandPalm";
+    vm.instagram = "https://www.instagram.com/pineandpalmbrew/";
   });
   
 })();
