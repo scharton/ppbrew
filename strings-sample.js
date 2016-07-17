8580d422a0e22ffbd3c7f08507ec3111ae5e8b55
Derek Scharton <dscharton@financialengines.com>
2016-07-16 20:51:53 -0700

    abstract strings to strings.js for more generic on github

---
+++ strings-sample.js
@@ -0,0 +1,18 @@
+(function() {
+   angular.module('app')
+       .value('strings',
+           {
+               name: "Pine and Palm Brewing",
+               address: {
+                   street: "352 W. Bedford Ave., #111",
+                   city: "Fresno",
+                   state: "CA",
+                   zip: "93711"
+               },
+               phone: "(559) 970-758",
+               email: "pineandpalmbrewing@gmail.com",
+               facebook: "https://www.facebook.com/PineandPalm",
+               instagram: "https://www.instagram.com/pineandpalmbrew/"
+           }
+       );
+})();
